1313ce3ecf1ef9593869c125b48804131c4c4b20
Helen Rong <helen.rong@nav.no>
2025-04-30 17:03:32 +0200

    databank

--- models/BT_statistikk_bank/dim_bt_periode.sql
+++ models/BT_statistikk_bank/dim_bt_periode.sql
@@ -7,5 +7,5 @@
 select aar, aar_kvartal, kvartal, kvartal_besk, forste_dato_i_perioden, siste_dato_i_perioden
 from {{ source('bt_statistikk_bank_dt_kodeverk', 'dim_tid') }}
 where gyldig_flagg = 1
-and dim_nivaa = 4 --På kvartal nivå
-and aar between 2015 and 2024 --Test
+and dim_nivaa = 4 --Kvartal nivå
+and aar between to_char(sysdate, 'yyyy') - 9 and to_char(sysdate, 'yyyy') --Hent ut siste 10 år
