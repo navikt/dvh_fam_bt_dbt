b5a2dc8cd5390da304f12af50785d539aec71836
Helen Rong <helen.rong@nav.no>
2025-08-20 14:44:17 +0200

    La til 255 i datatype av fam_bt_fagsak

--- models/BT_utpakking/fam_bt_fagsak.sql
+++ models/BT_utpakking/fam_bt_fagsak.sql
@@ -4,83 +4,83 @@
     )
 }}
 with barnetrygd_meta_data as (
-  select * from {{ref ('bt_meldinger_til_aa_pakke_ut')}}
+    select * from {{ref ('bt_meldinger_til_aa_pakke_ut')}}
 ),
 
-bt_person AS (
-  SELECT * FROM {{ ref ('fam_bt_person') }}
+bt_person as (
+    select * from {{ ref ('fam_bt_person') }}
 ),
 
 pre_final as (
-select * from barnetrygd_meta_data,
-  json_table(melding, '$'
-    COLUMNS (
-          behandling_opprinnelse    VARCHAR2 PATH '$.behandlingOpprinnelse'
-         ,behandling_type           VARCHAR2 PATH '$.behandlingTypeV2'
-         ,fagsak_id                 VARCHAR2 PATH '$.fagsakId'
-         ,behandlings_id            VARCHAR2 PATH '$.behandlingsId'
-         ,fagsak_type               VARCHAR2 PATH '$.fagsakType'
-         ,tidspunkt_vedtak          VARCHAR2 PATH '$.tidspunktVedtak'
-         ,enslig_forsørger          VARCHAR2 PATH '$.ensligForsørger'
-         ,kategori                  VARCHAR2 PATH '$.kategoriV2'
-         ,underkategori             VARCHAR2 PATH '$.underkategoriV2'
-         ,funksjonell_id            VARCHAR2 PATH '$.funksjonellId'
-         ,person_ident              VARCHAR2 PATH '$.personV2[*].personIdent'
-         ,behandling_Årsak           VARCHAR2 PATH '$.behandlingÅrsakV2'
-         )
-        ) j
+    select *
+    from barnetrygd_meta_data,
+    json_table(melding, '$'
+        columns (
+            behandling_opprinnelse varchar2(255) path '$.behandlingOpprinnelse'
+           ,behandling_type        varchar2(255) path '$.behandlingTypeV2'
+           ,fagsak_id              varchar2(255) path '$.fagsakId'
+           ,behandlings_id         varchar2(255) path '$.behandlingsId'
+           ,fagsak_type            varchar2(255) path '$.fagsakType'
+           ,tidspunkt_vedtak       varchar2(255) path '$.tidspunktVedtak'
+           ,enslig_forsørger       varchar2(255) path '$.ensligForsørger'
+           ,kategori               varchar2(255) path '$.kategoriV2'
+           ,underkategori          varchar2(255) path '$.underkategoriV2'
+           ,funksjonell_id         varchar2(255) path '$.funksjonellId'
+           ,person_ident           varchar2(255) path '$.personV2[*].personIdent'
+           ,behandling_årsak       varchar2(255) path '$.behandlingÅrsakV2'
+        )
+    ) j
 ),
 
 final as (
-  select
-    p.behandling_opprinnelse
-    ,p.behandling_type
-    ,p.fagsak_id
-    ,p.behandlings_id
-    ,p.fagsak_type
-    ,CASE
-      WHEN LENGTH(tidspunkt_vedtak) = 25 THEN CAST(to_timestamp_tz(tidspunkt_vedtak, 'yyyy-mm-dd"T"hh24:mi:ss TZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
-      ELSE CAST(to_timestamp_tz(tidspunkt_vedtak, 'FXYYYY-MM-DD"T"HH24:MI:SS.FXFF3TZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
-      END tidspunkt_vedtak
-    ,CASE
-      WHEN p.enslig_forsørger = 'false' THEN '0'
-      ELSE '1'
-      END AS enslig_forsørger
-    ,p.kategori
-    ,p.underkategori
-    ,'BT' AS kildesystem
-    ,sysdate AS lastet_dato
-    ,p.funksjonell_id
-    ,p.behandling_Årsak
-    ,p.person_ident
-    ,p.kafka_offset
-    ,p.kafka_mottatt_dato
-    ,pk_bt_meta_data as FK_BT_META_DATA
-    ,per.pk_bt_person as FK_BT_PERSON
-  from pre_final p
-  join bt_person per
-  on p.kafka_offset = per.kafka_offset
-  where per.soker_flagg = 1
+    select
+        p.behandling_opprinnelse
+       ,p.behandling_type
+       ,p.fagsak_id
+       ,p.behandlings_id
+       ,p.fagsak_type
+       ,case
+            when length(tidspunkt_vedtak) = 25 then cast(to_timestamp_tz(tidspunkt_vedtak, 'yyyy-mm-dd"T"hh24:mi:ss TZH:TZM') at time zone 'Europe/Belgrade' as timestamp)
+            else cast(to_timestamp_tz(tidspunkt_vedtak, 'FXYYYY-MM-DD"T"HH24:MI:SS.FXFF3TZH:TZM') at time zone 'Europe/Belgrade' as timestamp)
+        end tidspunkt_vedtak
+       ,case
+            when p.enslig_forsørger = 'false' then '0'
+            else '1'
+        end as enslig_forsørger
+       ,p.kategori
+       ,p.underkategori
+       ,'BT' as kildesystem
+       ,sysdate as lastet_dato
+       ,p.funksjonell_id
+       ,p.behandling_årsak
+       ,p.person_ident
+       ,p.kafka_offset
+       ,p.kafka_mottatt_dato
+       ,pk_bt_meta_data as fk_bt_meta_data
+       ,per.pk_bt_person as fk_bt_person
+    from pre_final p
+    join bt_person per
+    on p.kafka_offset = per.kafka_offset
+    where per.soker_flagg = 1
 )
 
 select
-    dvh_fambt_kafka.hibernate_sequence.nextval as PK_BT_FAGSAK
-    ,FK_BT_PERSON
-    ,FK_BT_META_DATA
-    ,BEHANDLING_OPPRINNELSE
-    ,BEHANDLING_TYPE
-    ,FAGSAK_ID
-    ,FUNKSJONELL_ID
-    ,BEHANDLINGS_ID
-    ,TIDSPUNKT_VEDTAK
-    ,ENSLIG_FORSØRGER
-    ,KATEGORI
-    ,UNDERKATEGORI
-    ,KAFKA_OFFSET
-    ,KILDESYSTEM
-    ,LASTET_DATO
-    ,BEHANDLING_ÅRSAK
-    ,FAGSAK_TYPE
-    ,kafka_mottatt_dato
+    dvh_fambt_kafka.hibernate_sequence.nextval as pk_bt_fagsak
+   ,fk_bt_person
+   ,fk_bt_meta_data
+   ,behandling_opprinnelse
+   ,behandling_type
+   ,fagsak_id
+   ,funksjonell_id
+   ,behandlings_id
+   ,tidspunkt_vedtak
+   ,enslig_forsørger
+   ,kategori
+   ,underkategori
+   ,kafka_offset
+   ,kildesystem
+   ,lastet_dato
+   ,behandling_årsak
+   ,fagsak_type
+   ,kafka_mottatt_dato
 from final
-
