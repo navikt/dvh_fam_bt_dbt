ea044b7bf08d47e3edf7b9c7312778e88a08554c
Helen Rong <helen.rong@nav.no>
2025-08-20 15:42:53 +0200

    La til 255 i datatype av fam_bt_utbetaling

--- models/BT_utpakking/fam_bt_utbetaling.sql
+++ models/BT_utpakking/fam_bt_utbetaling.sql
@@ -5,54 +5,55 @@
 }}
 
 with barnetrygd_meta_data as (
-  select * from {{ref ('bt_meldinger_til_aa_pakke_ut')}}
+    select * from {{ref ('bt_meldinger_til_aa_pakke_ut')}}
 ),
 
-bt_fagsak AS (
-  SELECT * FROM {{ ref ('fam_bt_fagsak') }}
+bt_fagsak as (
+    select * from {{ ref ('fam_bt_fagsak') }}
 ),
 
 pre_final as (
-select * from barnetrygd_meta_data,
-  json_table(melding, '$'
-    COLUMNS (
-      behandlings_id     VARCHAR2 PATH '$.behandlingsId',
-         NESTED             PATH '$.utbetalingsperioderV2[*]'
-         COLUMNS (
-         utbetalt_per_mnd   VARCHAR2 PATH '$.utbetaltPerMnd'
-        ,stønad_fom         VARCHAR2 PATH '$.stønadFom'
-        ,stønad_tom         VARCHAR2 PATH '$.stønadTom'
-        ,hjemmel            VARCHAR2 PATH '$.hjemmel'
+    select *
+    from barnetrygd_meta_data,
+    json_table(melding, '$'
+        columns (
+            behandlings_id varchar2(255) path '$.behandlingsId',
+            nested path '$.utbetalingsperioderV2[*]'
+            columns (
+                utbetalt_per_mnd   varchar2(255) path '$.utbetaltPerMnd'
+               ,stønad_fom         varchar2(255) path '$.stønadFom'
+               ,stønad_tom         varchar2(255) path '$.stønadTom'
+               ,hjemmel            varchar2(255) path '$.hjemmel'
+            )
         )
-      )
     ) j
     where stønad_fom is not null
       --where json_value (melding, '$.utbetalingsperioderV2.size()' )> 0
 ),
 
 final as (
-  select
-    p.behandlings_id,
-    p.utbetalt_per_mnd,
-    p.stønad_fom,
-    p.stønad_tom,
-    p.hjemmel,
-    p.kafka_offset,
-    p.kafka_mottatt_dato,
-    pk_BT_FAGSAK as FK_BT_FAGSAK
-  from pre_final p
-  join bt_fagsak b
-  on p.kafka_offset = b.kafka_offset
+    select
+        p.behandlings_id,
+        p.utbetalt_per_mnd,
+        p.stønad_fom,
+        p.stønad_tom,
+        p.hjemmel,
+        p.kafka_offset,
+        p.kafka_mottatt_dato,
+        pk_bt_fagsak as fk_bt_fagsak
+    from pre_final p
+    join bt_fagsak b
+    on p.kafka_offset = b.kafka_offset
 )
 
 select
-  dvh_fambt_kafka.hibernate_sequence.nextval PK_BT_UTBETALING
-  ,UTBETALT_PER_MND
-  ,TO_DATE(STØNAD_FOM, 'YYYY-MM-DD') STØNAD_FOM
-  ,TO_DATE(STØNAD_TOM, 'YYYY-MM-DD') STØNAD_TOM
-  ,HJEMMEL
-  ,FK_BT_FAGSAK
-  ,KAFKA_OFFSET
-  ,BEHANDLINGS_ID
-  ,localtimestamp AS lastet_dato
+    dvh_fambt_kafka.hibernate_sequence.nextval pk_bt_utbetaling
+   ,utbetalt_per_mnd
+   ,to_date(stønad_fom, 'YYYY-MM-DD') stønad_fom
+   ,to_date(stønad_tom, 'YYYY-MM-DD') stønad_tom
+   ,hjemmel
+   ,fk_bt_fagsak
+   ,kafka_offset
+   ,behandlings_id
+   ,localtimestamp as lastet_dato
 from final
