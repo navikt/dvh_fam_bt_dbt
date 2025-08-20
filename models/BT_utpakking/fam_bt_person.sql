81220bea7e9fa1898231aa21eda88dc7901e1d45
Helen Rong <helen.rong@nav.no>
2025-08-20 15:14:13 +0200

    La til 255 i datatype av fam_bt_person

--- models/BT_utpakking/fam_bt_person.sql
+++ models/BT_utpakking/fam_bt_person.sql
@@ -5,79 +5,80 @@
 }}
 
 with barnetrygd_meta_data as (
-  select * from {{ref ('bt_meldinger_til_aa_pakke_ut')}}
+    select * from {{ref ('bt_meldinger_til_aa_pakke_ut')}}
 ),
 
 pre_final_fagsak_person as (
-select * from barnetrygd_meta_data,
-  json_table(melding, '$'
-    COLUMNS (
-        behandlings_id            VARCHAR2 PATH '$.behandlingsId'
-        ,person_ident              VARCHAR2 PATH '$.personV2[*].personIdent'
-        ,rolle                     VARCHAR2 PATH '$.personV2[*].rolle'
-        ,bostedsland               VARCHAR2 PATH '$.personV2[*].bostedsland'
-        ,delingsprosent_ytelse     VARCHAR2 PATH '$.personV2[*].delingsprosentYtelse'
-         )
-        ) j
+    select *
+    from barnetrygd_meta_data,
+    json_table(melding, '$'
+        columns (
+            behandlings_id            varchar2(255) path '$.behandlingsId'
+           ,person_ident              varchar2(255) path '$.personV2[*].personIdent'
+           ,rolle                     varchar2(255) path '$.personV2[*].rolle'
+           ,bostedsland               varchar2(255) path '$.personV2[*].bostedsland'
+           ,delingsprosent_ytelse     varchar2(255) path '$.personV2[*].delingsprosentYtelse'
+        )
+    ) j
 ),
 
 pre_final_utbet_det_person as (
-select * from barnetrygd_meta_data,
-  json_table(melding, '$'
-    COLUMNS (
-      behandlings_id            VARCHAR2 PATH '$.behandlingsId',
-      NESTED                    PATH '$.utbetalingsperioderV2[*].utbetalingsDetaljer[*]'
-      COLUMNS (
-      person_ident               VARCHAR2 PATH '$.person.personIdent'
-      ,rolle                     VARCHAR2 PATH '$.person.rolle'
-      ,bostedsland               VARCHAR2 PATH '$.person.bostedsland'
-      ,delingsprosent_ytelse      VARCHAR2 PATH '$.person.delingsprosentYtelse'
+    select *
+    from barnetrygd_meta_data,
+    json_table(melding, '$'
+        columns (
+            behandlings_id varchar2(255) path '$.behandlingsId',
+            nested path '$.utbetalingsperioderV2[*].utbetalingsDetaljer[*]'
+            columns (
+                person_ident            varchar2(255) path '$.person.personIdent'
+               ,rolle                   varchar2(255) path '$.person.rolle'
+               ,bostedsland             varchar2(255) path '$.person.bostedsland'
+               ,delingsprosent_ytelse   varchar2(255) path '$.person.delingsprosentYtelse'
+            )
         )
-      )) j
-      where person_ident is not null
-      --where json_value (melding, '$.utbetalingsperioderV2.utbetalingsDetaljer.size()' )> 0
+    ) j
+    where person_ident is not null
+    --where json_value (melding, '$.utbetalingsperioderV2.utbetalingsDetaljer.size()' )> 0
 
 ),
 
 final_fagsak_person as (
-  select
-    person_ident
-    ,nvl(b.fk_person1, -1) fk_person1
-    ,behandlings_id
-    ,rolle
-    ,bostedsland
-    ,delingsprosent_ytelse
-    ,kafka_offset
-    ,KAFKA_MOTTATT_DATO
-    ,1 as soker_flagg
-  from
-    pre_final_fagsak_person
-  left outer join dt_person.ident_off_id_til_fk_person1 b on
-    pre_final_fagsak_person.person_ident=b.off_id
-    and b.gyldig_fra_dato<=pre_final_fagsak_person.kafka_mottatt_dato
-    and b.gyldig_til_dato>=kafka_mottatt_dato
+    select
+        person_ident
+       ,nvl(b.fk_person1, -1) fk_person1
+       ,behandlings_id
+       ,rolle
+       ,bostedsland
+       ,delingsprosent_ytelse
+       ,kafka_offset
+       ,kafka_mottatt_dato
+       ,1 as soker_flagg
+    from pre_final_fagsak_person
+    left outer join dt_person.ident_off_id_til_fk_person1 b
+    on pre_final_fagsak_person.person_ident = b.off_id
+    and b.gyldig_fra_dato <= pre_final_fagsak_person.kafka_mottatt_dato
+    and b.gyldig_til_dato >= kafka_mottatt_dato
     --and b.skjermet_kode=0
 ),
 
 final_utbet_det_person as (
-  select
-    person_ident
-    ,nvl(b.fk_person1, -1) fk_person1
-    ,behandlings_id
-    ,rolle
-    ,bostedsland
-    ,delingsprosent_ytelse
-    ,kafka_offset
-    ,KAFKA_MOTTATT_DATO
-    ,0 as soker_flagg
-  from
-    pre_final_utbet_det_person
-  left outer join dt_person.ident_off_id_til_fk_person1 b on
-    pre_final_utbet_det_person.person_ident=b.off_id
-    and b.gyldig_fra_dato<=pre_final_utbet_det_person.kafka_mottatt_dato
-    and b.gyldig_til_dato>=kafka_mottatt_dato
+    select
+        person_ident
+       ,nvl(b.fk_person1, -1) fk_person1
+       ,behandlings_id
+       ,rolle
+       ,bostedsland
+       ,delingsprosent_ytelse
+       ,kafka_offset
+       ,kafka_mottatt_dato
+       ,0 as soker_flagg
+    from pre_final_utbet_det_person
+    left outer join dt_person.ident_off_id_til_fk_person1 b
+    on pre_final_utbet_det_person.person_ident = b.off_id
+    and b.gyldig_fra_dato <= pre_final_utbet_det_person.kafka_mottatt_dato
+    and b.gyldig_til_dato >= kafka_mottatt_dato
     --and b.skjermet_kode=0
-  where person_ident is not null
+    where person_ident is not null
 ),
 
 final as (
@@ -87,27 +88,23 @@ final as (
 )
 
 select
-  dvh_fambt_kafka.hibernate_sequence.nextval as PK_BT_PERSON
-  ,cast(null as varchar2(30)) ANNENPART_BOSTEDSLAND
-  ,cast(null as varchar2(30)) ANNENPART_PERSONIDENT
-  ,cast(null as varchar2(30)) ANNENPART_STATSBORGERSKAP
-  ,cast(BOSTEDSLAND as VARCHAR2(255 CHAR)) BOSTEDSLAND
-  ,cast(null as varchar2(30)) DELINGSPROSENT_OMSORG
-  ,DELINGSPROSENT_YTELSE
-  ,case when fk_person1 = -1 then person_ident
-        else null
-   end person_ident
-  ,cast(null as varchar2(30)) PRIMÆRLAND
-  ,ROLLE
-  ,cast(null as varchar2(30)) SEKUNDÆRLAND
-  ,FK_PERSON1
-  ,KAFKA_OFFSET
-  ,BEHANDLINGS_ID
-  ,localtimestamp AS lastet_dato
-  ,localtimestamp AS OPPDATERT_DATO
-  ,soker_flagg
+    dvh_fambt_kafka.hibernate_sequence.nextval as pk_bt_person
+   ,cast(null as varchar2(30)) annenpart_bostedsland
+   ,cast(null as varchar2(30)) annenpart_personident
+   ,cast(null as varchar2(30)) annenpart_statsborgerskap
+   ,cast(bostedsland as varchar2(255 char)) bostedsland
+   ,cast(null as varchar2(30)) delingsprosent_omsorg
+   ,delingsprosent_ytelse
+   ,case when fk_person1 = -1 then person_ident
+         else null
+    end person_ident
+   ,cast(null as varchar2(30)) primærland
+   ,rolle
+   ,cast(null as varchar2(30)) sekundærland
+   ,fk_person1
+   ,kafka_offset
+   ,behandlings_id
+   ,localtimestamp as lastet_dato
+   ,localtimestamp as oppdatert_dato
+   ,soker_flagg
 from final
-
-
-
-
